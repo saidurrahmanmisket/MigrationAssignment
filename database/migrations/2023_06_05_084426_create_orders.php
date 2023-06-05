acc9db8fcc8d445e2532898588a0f63e20b00148
saidurrahmanmisket <mdsaidurrahmanmisket@gmail.com>
2023-06-05 16:39:00 +0600

    first commit

---
+++ database/migrations/2023_06_05_084426_create_orders.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('product_id');
+
+            //f-key
+            //relationship with product table
+
+            $table->foreign('product_id')->references('id')->on('products')
+            ->restrictOnDelete()
+            ->cascadeOnUpdate();
+
+            $table->integer('quantity');
+
+            $table->timestamp('create_at')->useCurrent();
+            $table->timestamp('update_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
